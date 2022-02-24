a158d772ea9a985af46bc4c261b2c17cd18d3686
Vivek Kumar Singh <64981263+singhvivekkumar@users.noreply.github.com>
2022-02-24 22:07:47 +0530

    fix running problem

--- Diabetes Doctor - Deployment.py
+++ Diabetes Doctor - Deployment.py
@@ -7,11 +7,12 @@ import pickle
 df = pd.read_csv('kaggle_diabetes.csv')
 
 # Renaming DiabetesPedigreeFunction as DPF
-df = df.rename(columns={'DiabetesPedigreeFunction':'DPF'})
+df = df.rename(columns={'DiabetesPedigreeFunction': 'DPF'})
 
 # Replacing the 0 values from ['Glucose','BloodPressure','SkinThickness','Insulin','BMI'] by NaN
 df_copy = df.copy(deep=True)
-df_copy[['Glucose','BloodPressure','SkinThickness','Insulin','BMI']] = df_copy[['Glucose','BloodPressure','SkinThickness','Insulin','BMI']].replace(0,np.NaN)
+df_copy[['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI']] = df_copy[
+    ['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI']].replace(0, np.NaN)
 
 # Replacing NaN value by mean, median depending upon distribution
 df_copy['Glucose'].fillna(df_copy['Glucose'].mean(), inplace=True)
@@ -22,15 +23,17 @@ df_copy['BMI'].fillna(df_copy['BMI'].median(), inplace=True)
 
 # Model Building
 from sklearn.model_selection import train_test_split
+
 X = df.drop(columns='Outcome')
 y = df['Outcome']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
 
 # Creating Random Forest Model
 from sklearn.ensemble import RandomForestClassifier
+
 classifier = RandomForestClassifier(n_estimators=20)
 classifier.fit(X_train, y_train)
 
 # Creating a pickle file for the classifier
-filename = 'diabetes-prediction-rfc-model.pkl'
+filename = 'diabetes-doctor-rfc-model.pkl'
 pickle.dump(classifier, open(filename, 'wb'))
--- app.py
+++ app.py
@@ -4,14 +4,16 @@ import pickle
 import numpy as np
 
 # Load the Random Forest CLassifier model
-filename = 'diabetes-prediction-rfc-model.pkl'
+filename = 'diabetes-doctor-rfc-model.pkl'
 classifier = pickle.load(open(filename, 'rb'))
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-	return render_template('index.html')
+    return render_template('index.html')
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -24,11 +26,12 @@ def predict():
         bmi = float(request.form['bmi'])
         dpf = float(request.form['dpf'])
         age = int(request.form['age'])
-        
+
         data = np.array([[preg, glucose, bp, st, insulin, bmi, dpf, age]])
         my_prediction = classifier.predict(data)
-        
+
         return render_template('result.html', prediction=my_prediction)
 
+
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
